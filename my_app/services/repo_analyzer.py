b408750d07c78fc6efec7f2a4825bfa8c562d834
Mohamed Nour <alkhalafmuhamade@gmail.com>
2024-04-15 15:53:58 +0300

    fixed the errors in the repo_analyzer.py file regarding the repo deletion.

--- my_app/services/repo_analyzer.py
+++ my_app/services/repo_analyzer.py
@@ -1,12 +1,20 @@
 import shutil
+import os
+import stat
 from my_app.services.repo_cloner import repo_cloner 
 from my_app.services.java_analyzer import analyze_java_files_in_directory 
+
+def remove_readonly(func, path, exc_info):
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWRITE)
+    func(path)
 
 def repo_analyzer(repo_url):
     destination = repo_cloner(repo_url) 
     analyze_java_files_in_directory(destination, repo_url)
     try:
-        shutil.rmtree(destination)
+        shutil.rmtree(destination, onerror=remove_readonly)
+        print(f"Removed repo {destination}")
     except Exception as e:
         print(f"Error removing directory {destination}: {e}")
 
