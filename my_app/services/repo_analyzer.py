54b678db506e073594169cef8a14011f02940cab
Mohamed Nour <alkhalafmuhamade@gmail.com>
2024-04-15 13:20:39 +0300

    added the functionality to delete the cloned repo after the analysis is done added gitignore

--- my_app/services/repo_analyzer.py
+++ my_app/services/repo_analyzer.py
@@ -1,7 +1,12 @@
+import shutil
 from my_app.services.repo_cloner import repo_cloner 
 from my_app.services.java_analyzer import analyze_java_files_in_directory 
 
 def repo_analyzer(repo_url):
     destination = repo_cloner(repo_url) 
     analyze_java_files_in_directory(destination, repo_url)
+    try:
+        shutil.rmtree(destination)
+    except Exception as e:
+        print(f"Error removing directory {destination}: {e}")
 
